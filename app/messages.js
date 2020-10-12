2c8d0012f89e2d440a6b752a9dedc990d0e80b09
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-10-12 13:07:35 +0600

    Добавлена проверка на валидность даты

--- app/messages.js
+++ app/messages.js
@@ -1,19 +1,27 @@
 const router = require("express").Router();
 const db = require("../fileDB");
 
+let errorMessage = {
+  error: ''
+};
 
 router.get("/", (req, res) => {
-
   const messages = db.getItems();
-
   if (req.query.dateTime) {
     const dateTime = req.query.dateTime;
-    const index = messages.findIndex(messages => messages.dateTime === dateTime);
-    const dateTimeArr = [];
-    for (let i = index + 1; i < messages.length; i++) {
-      dateTimeArr.push(messages[i]);
-    };
-    res.send(dateTimeArr);
+    const date = new Date(req.query.dateTime);
+    if (isNaN(date.getDate())) {
+      res.status(400);
+      errorMessage.error = 'Invalid Date'
+      res.send(errorMessage);
+    } else {
+      const index = messages.findIndex(messages => messages.dateTime === dateTime);
+      const dateTimeArr = [];
+      for (let i = index + 1; i < messages.length; i++) {
+        dateTimeArr.push(messages[i]);
+      };
+      res.send(dateTimeArr);
+    }
   } else {
     res.send(messages);
   };
@@ -22,9 +30,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   if (req.body.message === "" || req.body.author === "") {
     res.status(400);
-    const errorMessage = {
-      error: 'Author and message must be present in the request',
-    };
+    errorMessage.error = 'Author and message must be present in the request';
     res.send(errorMessage);
   } else {
     const messages = db.addItem(req.body);
